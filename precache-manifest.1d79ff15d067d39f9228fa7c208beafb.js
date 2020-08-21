38446fc1339af3d4772967ac7a1ecdc13080a253
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-21 12:22:37 +0600

    Full Site

---
+++ precache-manifest.1d79ff15d067d39f9228fa7c208beafb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28296f5285184c4307878b9dbfbf342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd2999b1a240c674a10",
+    "url": "/static/css/2.51a53583.chunk.css"
+  },
+  {
+    "revision": "d4dd32839fe62f051a75",
+    "url": "/static/css/main.422f09e6.chunk.css"
+  },
+  {
+    "revision": "1fd2999b1a240c674a10",
+    "url": "/static/js/2.f949977f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f949977f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4dd32839fe62f051a75",
+    "url": "/static/js/main.4d628968.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1d4e58a42017753c6c8045a49e9a5d4a",
+    "url": "/static/media/jobbasket.1d4e58a4.png"
+  },
+  {
+    "revision": "dcc3eb0bde12697dc6dae6e69f678ae5",
+    "url": "/static/media/me.dcc3eb0b.jpg"
+  },
+  {
+    "revision": "77fcf14e895c14bc3efe1ea77df73a8d",
+    "url": "/static/media/photopoll.77fcf14e.png"
+  },
+  {
+    "revision": "2ee8ac8a9b9e75b4734ac2bdfc7412e5",
+    "url": "/static/media/pomodoro.2ee8ac8a.png"
+  }
+]);

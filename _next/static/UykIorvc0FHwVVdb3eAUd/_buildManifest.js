d567500651c10fc1867964ac4ec9474d6518345d
hpdipto <haris.dipto@gmail.com>
2022-04-30 16:24:22 +0600

    sites transferred to nextjs

---
+++ _next/static/UykIorvc0FHwVVdb3eAUd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/648-9c9f086e13b6ecd3.js","static/chunks/pages/index-93856f32337254d5.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
